4a2dd5b35a5c2c9c0056428f1a39e41ba926a939
adam-renner <reidadamrenner@gmail.com>
2023-07-30 16:02:37 -0400

    updates

---
+++ Pellet.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Pellet = (props) => {
+  // Define the pellet properties and rendering logic
+
+  return <div>{/* Render the pellet */}</div>;
+};
+
+export default Pellet;
---
+++ Snake.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Snake = (props) => {
+  // Define the snake properties and rendering logic
+
+  return (
+    <div>
+      {/* Render the snake segments */}
+    </div>
+  );
+};
+
+export default Snake;
